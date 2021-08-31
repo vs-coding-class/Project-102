8ceab1eae31499a48eeaea35bafba9d08e7f1719
vs-coding-class <65456951+vs-coding-class@users.noreply.github.com>
2021-08-31 13:47:47 -0400

    Add files via upload

---
+++ Project_102/Project_102.py
@@ -0,0 +1,25 @@
+import os
+
+def openIndividual(path):
+    os.system(f'open {path}')
+    print('Application is opened.')
+
+def openMultipleApplications(listName):
+    file = open('Application_Lists.txt', 'r')
+    for line in file:
+        names = line.split(', ')
+        if names[0] == listName:
+            for item in names[1:]:
+                os.system(f'open {item}')
+
+def main():
+    while True:
+        type = input("Please enter 'one' to open only one application or 'list' to open a set of applications: ")
+        if type == 'one':
+            oneFile = input('Please enter the name of the application you would like to open: ')
+            openIndividual(oneFile)
+        elif type == 'list':
+            setName = input('Please enter the name of the set of applications you would like to open: ')
+            openMultipleApplications(setName)
+
+main()
